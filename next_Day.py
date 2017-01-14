4b1c8c475fc721f6f34c4902eb5a898ef6637312
DIPTARK BOSE <diptark.bose@gmail.com>
2017-01-14 09:36:34 +0530

    Made next_Day

--- next_Day.py
+++ next_Day.py
@@ -8,7 +8,11 @@ def next_Day(year, month, date):
     if date<=29:
         return year, month, int(date)+1
     else:
-        return year, int(month)+1, 1
+        if month==12:
+            return int(year)+1, 1, 1
+        else:
+            return year, int(month)+1, 1
         
 year, month, day=next_Day(1997, 10, 1)
+print month
 
