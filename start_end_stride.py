5b5d5a71e6a7144197ef7ec5574dde186b16dcc2
DIPTARK BOSE <diptark.bose@gmail.com>
2017-01-19 11:09:38 +0530

    start_end_stride

---
+++ start_end_stride.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 11:08:40 2017
+
+@author: Diptark Bose
+"""
+
+my_list = range(1, 11) # List of numbers 1 - 10
+
+# Add your code below!
+print my_list[::2]
