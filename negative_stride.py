d5b2d1eecc5b26caead42020cbdb9cb005a0e4e1
DIPTARK BOSE <diptark.bose@gmail.com>
2017-01-19 11:12:24 +0530

    negative_stride

---
+++ negative_stride.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 11:11:33 2017
+
+@author: Diptark Bose
+"""
+
+my_list = range(1, 11)
+
+# Add your code below!
+backwards=my_list[::-1]
+print backwards
